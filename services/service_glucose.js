c95f6bc4cd868db2a062576204a9a1eb4706208d
Gabriel Moura <rafaelmoura2929@gmail.com>
2020-03-28 16:25:28 -0300

    Limpando respostas

--- services/service_glucose.js
+++ services/service_glucose.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 
 const Glucose = mongoose.model("Glucose");
- 
+
 module.exports = {
     async create_glucose(req, res) {
         const glucose = await Glucose.create(req.body);
-        res.send({passed: glucose,});
+        res.send({ glucose });
     },
     async list_glucose(req, res) {
-        if(req.query.tagId){
-            const glucose = await Glucose.find({"_id":req.query.tagId});
-            return res.send({glucose}); 
+        if (req.query.tagId) {
+            const glucose = await Glucose.find({ "_id": req.query.tagId });
+            return res.send({ glucose });
         }
         const glucose = await Glucose.find();
-        return res.send({glucose});
+        return res.send({ glucose });
     },
 }
--- services/service_paciente.js
+++ services/service_paciente.js
@@ -1,18 +1,18 @@
 const mongoose = require("mongoose");
 
 const Paciente = mongoose.model("Paciente");
- 
+
 module.exports = {
     async create_paciente(req, res) {
         const paciente = await Paciente.create(req.body);
-        res.send({passed: paciente,});
+        res.send({ paciente });
     },
     async list_paciente(req, res) {
-        if(req.query.tagId){
-            const paciente = await Paciente.find({"_id":req.query.tagId});
-            return res.send({paciente}); 
+        if (req.query.tagId) {
+            const paciente = await Paciente.find({ "_id": req.query.tagId });
+            return res.send({ paciente });
         }
         const paciente = await Paciente.find();
-        return res.send({paciente});
+        return res.send({ paciente });
     },
 }
