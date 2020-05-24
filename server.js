320ba6a11a547cef8d1d38600c329582ae0fb341
Rafael Moura <rafaelmoura2929@gmail.com>
2020-05-24 17:47:39 -0300

    Erro cors

--- server.js
+++ server.js
@@ -10,6 +10,16 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use(function(req, res, next) {
+  var allowedOrigins = ['http://127.0.0.1:8080', 'http://localhost:8080', 'https://glycon.herokuapp.com/'];
+  var origin = req.headers.origin;
+  if(allowedOrigins.indexOf(origin) > -1){
+       res.setHeader('Access-Control-Allow-Origin', origin);
+  }
+ 
+  return next();
+});
+
 // Iniciando o DB
 mongoose.connect("mongodb://usuario:usuario@mongodbblackbook-shard-00-00-zdqhv.azure.mongodb.net:27017,mongodbblackbook-shard-00-01-zdqhv.azure.mongodb.net:27017,mongodbblackbook-shard-00-02-zdqhv.azure.mongodb.net:27017/test?ssl=true&replicaSet=MongoDbBlackBook-shard-0&authSource=admin&retryWrites=true&w=majority", {
   useNewUrlParser: true, 
