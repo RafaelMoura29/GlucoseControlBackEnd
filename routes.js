e193f39632bd008f87bbe2861d7ab9169b760f2a
Rafael Moura <rafaelmoura2929@gmail.com>
2020-05-24 22:38:34 -0300

    CORS

--- routes.js
+++ routes.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const routes = express.Router();
+const cors = require("cors")
 
 /* Paciente  */
 const paciente = require('./services/service_paciente')
-routes.post('/paciente', paciente.create_paciente);
-routes.get('/paciente', paciente.list_paciente);
-routes.put('/paciente', paciente.update_paciente);
+routes.post('/paciente',cors(), paciente.create_paciente);
+routes.get('/paciente',cors(), paciente.list_paciente);
+routes.put('/paciente',cors(), paciente.update_paciente);
 
 /* glicemia */
 const glicemia = require('./services/service_glicemia')
-routes.post('/glicemia', glicemia.create_glicemia);
+routes.post('/glicemia',cors(), glicemia.create_glicemia);
 
 /* Aplicação */
 const aplicacao = require('./services/service_aplicacao')
-routes.post('/aplicacao', aplicacao.create_aplicacao)
+routes.post('/aplicacao',cors(), aplicacao.create_aplicacao)
 
 routes.get('/', (req, res) => {
     res.sendfile('./views/index.html')
