de3a948852abbe996fa3a95d7a07741d75f6026e
Rafael Moura <rafaelmoura2929@gmail.com>
2020-05-24 22:31:37 -0300

    CORS

--- server.js
+++ server.js
@@ -1,24 +1,17 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const requireDir = require("require-dir");
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const requireDir = require("require-dir")
 
 // Iniciando o app
-const app = express();
+const app = express()
 
 // permitir receber JSON nas requisições
-app.use(express.json());
-app.use(cors());
-
-app.use(function(req, res, next) {
-  var allowedOrigins = ['http://127.0.0.1:8080', 'http://localhost:8080', 'https://glycon.herokuapp.com/'];
-  var origin = req.headers.origin;
-  if(allowedOrigins.indexOf(origin) > -1){
-       res.setHeader('Access-Control-Allow-Origin', origin);
-  }
- 
-  return next();
-});
+app.use(express.json())
+app.use(cors({
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+}))
 
 // Iniciando o DB
 mongoose.connect("mongodb://usuario:usuario@mongodbblackbook-shard-00-00-zdqhv.azure.mongodb.net:27017,mongodbblackbook-shard-00-01-zdqhv.azure.mongodb.net:27017,mongodbblackbook-shard-00-02-zdqhv.azure.mongodb.net:27017/test?ssl=true&replicaSet=MongoDbBlackBook-shard-0&authSource=admin&retryWrites=true&w=majority", {
@@ -29,7 +22,7 @@ mongoose.connect("mongodb://usuario:usuario@mongodbblackbook-shard-00-00-zdqhv.a
     console.log(error)
   })
 
-requireDir("./models");
-app.use( require("./routes"));
+requireDir("./models")
+app.use( require("./routes"))
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080)
