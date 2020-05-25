956fdbccf737ee23753c4cbc67c723a49dcd66b5
Rafael Moura <rafaelmoura2929@gmail.com>
2020-05-24 22:35:40 -0300

    CORS

--- server.js
+++ server.js
@@ -8,11 +8,8 @@ const app = express()
 
 // permitir receber JSON nas requisições
 app.use(express.json())
-app.use(cors({
-  origin: "*",
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-}))
-
+app.use(cors())
+app.options('*', cors())
 // Iniciando o DB
 mongoose.connect("mongodb://usuario:usuario@mongodbblackbook-shard-00-00-zdqhv.azure.mongodb.net:27017,mongodbblackbook-shard-00-01-zdqhv.azure.mongodb.net:27017,mongodbblackbook-shard-00-02-zdqhv.azure.mongodb.net:27017/test?ssl=true&replicaSet=MongoDbBlackBook-shard-0&authSource=admin&retryWrites=true&w=majority", {
   useNewUrlParser: true, 
