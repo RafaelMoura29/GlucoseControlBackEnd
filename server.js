8bba44f00f7f04535a7a8f46781c4a0366a11fbf
Rafael Moura <rafaelmoura2929@gmail.com>
2020-05-26 19:40:27 -0300

    Adi

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json())
 app.use(cors())
 
 // Iniciando o DB
-mongoose.connect("mongodb://usuario:usuario@mongodbblackbook-shard-00-00-zdqhv.azure.mongodb.net:27017,mongodbblackbook-shard-00-01-zdqhv.azure.mongodb.net:27017,mongodbblackbook-shard-00-02-zdqhv.azure.mongodb.net:27017/test?ssl=true&replicaSet=MongoDbBlackBook-shard-0&authSource=admin&retryWrites=true&w=majority", {
+mongoose.connect(process.env.API, {
   useNewUrlParser: true, 
   useUnifiedTopology: true
 })
