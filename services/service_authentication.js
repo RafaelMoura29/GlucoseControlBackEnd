0d54eebd61723e3ddf07fb0978573c0fc0bf82a5
Rafael Moura <rafaelmoura2929@gmail.com>
2020-06-14 15:33:47 -0300

    Adding verification of password and username in register user route

--- services/service_authentication.js
+++ services/service_authentication.js
@@ -1,8 +1,21 @@
 const mongoose = require("mongoose")
 const Usuario = mongoose.model('Usuario')
+const format = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/
 
 module.exports = {
   register(req, res) {
-    return res.send({"hello": "buddy"})
+    const { password, confirmPassword, username } = req.body
+
+    if (password !== confirmPassword) {
+      return res.status(406).send("Password and confirm password are not equal.")
+    } else if (password.length <= 8) {
+      return res.status(406).send("Password length is not accepted.")
+    } else if (format.test(password)) {
+      return res.status(406).send("Password does not accept special characters.")
+    } else if (username.length <= 8) {
+      return res.status(406).send("Username length is not accepted")
+    }
+
+    return res.send({ "hello": "buddy" })
   }
 }
