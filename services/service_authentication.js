0b62ca2764771e5851cbb58899f10fe81cf3e359
Rafael Moura <rafaelmoura2929@gmail.com>
2020-06-14 15:49:36 -0300

    Adding register of user

--- services/service_authentication.js
+++ services/service_authentication.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 const Usuario = mongoose.model('Usuario')
-const format = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/
+//const format = /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/
 
 module.exports = {
   register(req, res) {
@@ -10,12 +10,22 @@ module.exports = {
       return res.status(406).send("Password and confirm password are not equal.")
     } else if (password.length <= 8) {
       return res.status(406).send("Password length is not accepted.")
-    } else if (format.test(password)) {
-      return res.status(406).send("Password does not accept special characters.")
     } else if (username.length <= 8) {
       return res.status(406).send("Username length is not accepted")
     }
+    /* else if (format.test(password)) {
+      return res.status(406).send("Password does not accept special characters.")
+    }  */
 
-    return res.send({ "hello": "buddy" })
+    const newUser = new Usuario({ username, password })
+
+    newUser.save()
+      .then((response) => {
+        return res.status(201).send(response)
+      })
+      .catch((error) => {
+        return res.status(400).send(error)
+      })
+
   }
 }
