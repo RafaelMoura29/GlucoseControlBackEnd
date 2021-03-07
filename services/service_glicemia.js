77ebddddc70009acbe890f4e16de9120ffeae333
rafaelmoura29 <rafaelmoura2929@gmail.com>
2021-03-07 18:21:12 -0300

    chore: adding new propertie to glicemia schema

--- services/service_glicemia.js
+++ services/service_glicemia.js
@@ -2,16 +2,17 @@ const mongoose = require("mongoose");
 const Paciente = mongoose.model("Paciente");
 
 module.exports = {
-  salvarGlicemia(req, res) {
-    const { _idPaciente, ...glicemia } = req.body
-    Paciente.findOne({ _id: String(_idPaciente) })
-      .then((paciente) => {
-        paciente.glicemia.push(glicemia)
-        paciente.updateDate = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' })
-        paciente.save()
-          .then((response) => res.status(200).send(response.glicemia))
-          .catch((error) => res.status(400).send(error))
-      })
-      .catch((error) => res.status(400).send(error))
-  },
+    salvarGlicemia(req, res) {
+        const { _idPaciente, diabetes, ...glicemia } = req.body
+        Paciente.findOne({ _id: String(_idPaciente) })
+            .then((paciente) => {
+                paciente.glicemia.push(glicemia)
+                paciente.updateDate = new Date().toLocaleString('pt-BR', { timeZone: 'America/Sao_Paulo' })
+                paciente.diabetes = diabetes
+                paciente.save()
+                    .then((response) => res.status(200).send(response.glicemia))
+                    .catch((error) => res.status(400).send(error))
+            })
+            .catch((error) => res.status(400).send(error))
+    },
 }
